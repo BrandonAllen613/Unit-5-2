a8233b8f49aafcb6951ac37c4eba98e8a5f016c6
Brandon Allen <>
2021-05-02 18:27:05 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,23 @@
+<!DOCTYPEhtml>
+<!-- This a comment -->
+<html lang="en-ca">
+  <head>
+    <meta charset="utf-8">
+    <meta name="despriction"content="My first webpage, with image">
+    <meta name="keywords"content="MTHS, Website"> 
+    <meta name="author"content="Mr. Allen">
+    <meta name="viewport"content="width=device-width, intial scale=1. 0">
+    <title>="Brandon Wayne Allen",</title>
+        <body style= "background-colour:yellow">
+        <button type="button">Click Here!</button>
+        <p id="display">-</p>
+        <form action"" method"="GET"
+          <input type="text" id="fname" placeholder="Name"/>
+          <input type="button" id="btn" placeholder="Submit"/>
+  </form>
+  <head>
+  <body>
+  <input type="button" value="Click Here!" onclick="alert (Please Consider The Following Actions)"/>
+    <body>
+  </html> 
+         
